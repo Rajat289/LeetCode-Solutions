33ab8a266fc7ce58e97bd0927108913652709899
Rajat Kaware <rajatkaware2005@gmail.com>
2026-03-27 18:07:05 +0530

    Create 1393. Capital Gain-Loss.sql

---
+++ SQL/1393. Capital Gain-Loss.sql
@@ -0,0 +1,17 @@
+WITH TotalBought AS (
+    SELECT stock_name, SUM(price) AS spent
+    FROM Stocks
+    WHERE operation = 'Buy'
+    GROUP BY stock_name
+),
+TotalSold AS (
+    SELECT stock_name , SUM(price) AS gained
+    FROM Stocks
+    WHERE operation = 'Sell'
+    GROUP BY stock_name
+)
+
+SELECT B.stock_name, gained - spent AS capital_gain_loss
+FROM TotalBought AS B
+JOIN TotalSold AS S
+ON B.stock_name = S.stock_name
