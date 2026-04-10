8ccb125e22b6dc161542b63389b683195f808fa0
Rajat Kaware <rajatkaware2005@gmail.com>
2026-04-10 23:26:40 +0530

    Implement maximum subarray solution in Java

---
+++ Arrays/53. Maximum Subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] arr) {
+        int maxsum = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i =0 ; i<arr.length ; i++)
+       {
+        sum += arr[i];
+        maxsum=Math.max(maxsum,sum);
+        if(sum<0){
+            sum = 0;
+        }
+       } 
+       return maxsum;
+    }
+}
+    
