efdbc28fdb788bca6f8e824bba5e1e8cfb202c27
Rajat Kaware <rajatkaware2005@gmail.com>
2026-04-24 10:44:25 +0530

    Create 242. Valid Anagram.java

---
+++ Strings/242. Valid Anagram.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+
+public class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        
+        HashMap<Character, Integer> countMap = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            countMap.put(c, countMap.getOrDefault(c, 0) + 1);
+        }
+
+        
+        for (char c : t.toCharArray()) {
+            if (!countMap.containsKey(c) || countMap.get(c) == 0) {
+                return false;
+            }
+            countMap.put(c, countMap.get(c) - 1);
+        }
+
+        return true;
+    }
+}
