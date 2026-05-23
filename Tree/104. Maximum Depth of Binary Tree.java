05439cd4e0165c46f73db57f14bf5f333440b653
Rajat Kaware <rajatkaware2005@gmail.com>
2026-05-23 23:27:38 +0530

    Implement maxDepth function for binary tree

--- Tree/104. Maximum Depth of Binary Tree.java
+++ Tree/104. Maximum Depth of Binary Tree.java
@@ -1,1 +1,10 @@
-
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int leftheight=maxDepth(root.left);
+        int rightheight=maxDepth(root.right);
+        return 1+Math.max(leftheight , rightheight);
+    }
+}
