7bcc51cba16509027db0602b8b63cec36ceeec36
Rajat Kaware <rajatkaware2005@gmail.com>
2026-05-24 00:32:34 +0530

    Implement maxSatisfaction method to calculate result

--- Arrays/1402. Reducing Dishes.java
+++ Arrays/1402. Reducing Dishes.java
@@ -1,1 +1,22 @@
+class Solution {
+    public int maxSatisfaction(int[] satisfaction) {
+        
+        Arrays.sort(satisfaction);
 
+        int cumulativeSum = 0; 
+        int result = 0;         
+        for (int i = satisfaction.length - 1; i >= 0; i--) {
+            
+            cumulativeSum += satisfaction[i];
+
+            if (cumulativeSum <= 0) {
+                break;
+            }
+
+            
+            result += cumulativeSum;
+        }
+
+        return result;
+    }
+}
