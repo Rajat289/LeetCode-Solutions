cb9fec67ca7f5c0ac296fc3acf12264a989ea0fd
Rajat Kaware <rajatkaware2005@gmail.com>
2026-05-23 22:27:53 +0530

    Implement BST validation using helper function

--- Tree/98. Validate Binary Search Tree.java
+++ Tree/98. Validate Binary Search Tree.java
@@ -1,1 +1,20 @@
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        
+        return helper(root,null,null);
+    }
 
+    boolean helper(TreeNode node , TreeNode min , TreeNode max){
+        if(node == null)
+        return true;
+
+        if(min!= null && min.val >= node.val){
+            return false;
+        }
+        if(max != null && max.val <= node.val){
+            return false;
+        }
+        return helper(node.left , min , node)
+            && helper(node.right , node , max);
+    }
+}
