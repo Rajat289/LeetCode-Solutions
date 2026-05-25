b81902213b90a2e593411a52e94024aac2018ed5
Rajat Kaware <rajatkaware2005@gmail.com>
2026-05-25 23:03:26 +0530

    Update 110. Balanced Binary Tree.java

--- Depth-First Search/110. Balanced Binary Tree.java
+++ Depth-First Search/110. Balanced Binary Tree.java
@@ -1,1 +1,22 @@
-
+class Solution {
+    private int dfs(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int leftheight = dfs(root.left);
+        if(leftheight == -1){
+            return -1;
+        }
+        int rightheight = dfs(root.right);
+        if(rightheight == -1){
+            return -1;
+        }
+        if(Math.abs(leftheight - rightheight)>1){
+            return -1;
+        }
+        return 1+Math.max(leftheight , rightheight);
+    }
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root) != -1;
+    }
+}
