1c481217f18416c71ea4908f42df9790eed27dad
Rajat Kaware <rajatkaware2005@gmail.com>
2026-05-25 23:05:57 +0530

    Refactor minDepth method for better readability

--- Depth-First Search/111. Minimum Depth of Binary Tree.java
+++ Depth-First Search/111. Minimum Depth of Binary Tree.java
@@ -3,8 +3,10 @@ class Solution {
                 if(root == null)return 0;
                         int lh=minDepth(root.left);
                         int rh=minDepth(root.right);
-                        if(lh==0 || rh==0)return  1+Math.max(lh,rh);
-                        return 1+Math.min(lh,rh);
+                        if(lh==0 || rh==0)
+                                return  1+Math.max(lh,rh);
+                       
+                return 1+Math.min(lh,rh);
                 
         }
 }
