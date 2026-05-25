2b8cd3cae58150091297227b8b6df0844fde6786
Rajat Kaware <rajatkaware2005@gmail.com>
2026-05-25 22:56:41 +0530

    Implement Maximum Binary Tree construction

--- Tree/654. Maximum Binary Tree.java
+++ Tree/654. Maximum Binary Tree.java
@@ -1,1 +1,19 @@
-
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+        Stack<TreeNode> stack=new Stack<>();
+        for(int i:nums){
+            TreeNode cu=new TreeNode(i);
+            while(!stack.isEmpty() && stack.peek().val<i){
+            cu.left=stack.pop();
+        }
+        if(!stack.isEmpty()){
+            stack.peek().right=cu;
+        }
+        stack.push(cu);
+    }
+    while(stack.size()>1){
+        stack.pop();
+    }
+    return stack.peek();
+}
+}
