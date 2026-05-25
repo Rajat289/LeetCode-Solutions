40c9183fbed3175a2f80c5614af8b745182a3a4d
Rajat Kaware <rajatkaware2005@gmail.com>
2026-05-25 22:55:41 +0530

    Implement diameter calculation for binary tree

--- Tree/543. Diameter of Binary Tree.java
+++ Tree/543. Diameter of Binary Tree.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    int res=0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return res;
+    }
+    private int dfs(TreeNode root){
+        if(root == null)return 0;
+        int lh=dfs(root.left);
+        int rh=dfs(root.right);
+        res=Math.max(res, lh+rh);
+        return 1 + Math.max(lh,rh);
+    }
+}
