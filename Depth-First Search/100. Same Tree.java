5fa052833d2aaceda61f950ace9f3e75e0034ac7
Rajat Kaware <rajatkaware2005@gmail.com>
2026-05-26 08:28:13 +0530

    Update 100. Same Tree.java

--- Depth-First Search/100. Same Tree.java
+++ Depth-First Search/100. Same Tree.java
@@ -3,5 +3,6 @@ class Solution {
         if(p==null&&q==null)return true;
         if(p==null||q==null||p.val!=q.val)return false;
         return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        
     }
 }
