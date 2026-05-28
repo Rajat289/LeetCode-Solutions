dc574291dca3cbd7400db9f86035c5ec1371b22e
Rajat Kaware <rajatkaware2005@gmail.com>
2026-05-28 23:13:13 +0530

    Update 1009. Complement of Base 10 Integer.java

--- Bit Manipulation/1009. Complement of Base 10 Integer.java
+++ Bit Manipulation/1009. Complement of Base 10 Integer.java
@@ -1,1 +1,14 @@
-
+class Solution {
+    public int bitwiseComplement(int n) {
+        if(n==0)return 1;
+        int sum=0;
+        int i=0;
+        while(n>0){
+            int r = n%2;
+            sum += Math.pow(2,i)*(r^1);
+            n/=2;
+            i++;
+        }
+        return sum;
+    }
+}
