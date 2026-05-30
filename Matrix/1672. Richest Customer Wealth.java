245095f8a5d01fb7b9720c58c5d3374e55b1ee78
Rajat Kaware <rajatkaware2005@gmail.com>
2026-05-30 17:59:11 +0530

    Update 1672. Richest Customer Wealth.java

--- Matrix/1672. Richest Customer Wealth.java
+++ Matrix/1672. Richest Customer Wealth.java
@@ -1,1 +1,16 @@
-
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int ans = accounts[0][0];
+        for(int i = 0 ; i<accounts.length ; i++){
+            int sum = 0;
+            for(int j=0 ; j<accounts[i].length ; j++){
+                sum = sum+accounts[i][j];
+            }
+            if(sum > ans){
+                ans = sum;
+            }
+            
+       }
+       return ans;
+    }
+}
