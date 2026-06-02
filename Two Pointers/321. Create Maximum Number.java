a1f23b237ee96979021b27ebfed43240e1f2fac3
Rajat Kaware <rajatkaware2005@gmail.com>
2026-06-02 21:53:35 +0530

    Implement solution for creating maximum number from arrays

--- Two Pointers/321. Create Maximum Number.java
+++ Two Pointers/321. Create Maximum Number.java
@@ -1,1 +1,52 @@
+class Solution {
+    private int[] create(int[] a, int k) {
+        int n = a.length;
+        int[] st = new int[k];
+        int top = 0;
+        for (int i = 0; i < n; i++) {
+            while (top > 0 && a[i] > st[top - 1] && n - i + top - 1 >= k) {
+                top--;
+            }
+            if (top < k) {
+                st[top++] = a[i];
+            }
+        }
+        return st;
+    }
 
+    private boolean greater(int[] a, int i, int[] b, int j) {
+        while (i < a.length && j < b.length && a[i] == b[j]) {
+            i++;
+            j++;
+        }
+        return j == b.length || (i < a.length && a[i] > b[j]);
+    }
+
+    private int[] merge(int[] a, int[] b) {
+        int n = a.length, m = b.length;
+        int[] res = new int[n + m];
+        int i = 0, j = 0, r = 0;
+        while (i < n || j < m) {
+            if (greater(a, i, b, j)) {
+                res[r++] = a[i++];
+            } else {
+                res[r++] = b[j++];
+            }
+        }
+        return res;
+    }
+
+    public int[] maxNumber(int[] nums1, int[] nums2, int k) {
+        int n = nums1.length, m = nums2.length;
+        int[] res = new int[k];
+        for (int i = Math.max(0, k - m); i <= Math.min(k, n); i++) {
+            int[] x = create(nums1, i);
+            int[] y = create(nums2, k - i);
+            int[] v = merge(x, y);
+            if (greater(v, 0, res, 0)) {
+                res = v;
+            }
+        }
+        return res;
+    }
+}
