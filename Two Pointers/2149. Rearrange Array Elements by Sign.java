0fa9c7be5a18643096fc843c086d339ac6a33b02
Rajat Kaware <rajatkaware2005@gmail.com>
2026-06-15 16:51:30 +0530

    Update 2149. Rearrange Array Elements by Sign.java

--- Two Pointers/2149. Rearrange Array Elements by Sign.java
+++ Two Pointers/2149. Rearrange Array Elements by Sign.java
@@ -1,1 +1,23 @@
-
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int n=nums.length/2;
+        int posarr[]=new int[n];
+        int negarr[]=new int[n];
+        int ans[]=new int[2*n];
+        int p=0;
+        int q=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                posarr[p++]=nums[i];
+            }else{
+                negarr[q++]=nums[i];
+            }
+        }
+        int index=0;
+        for(int i=0;i<n;i++){
+            ans[index++]=posarr[i];
+            ans[index++]=negarr[i];
+        }
+        return ans;
+    }
+}
