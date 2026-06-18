14714339a1ae1358b9f19f4a15a02f3699ffec15
Rajat Kaware <rajatkaware2005@gmail.com>
2026-06-18 23:14:48 +0530

    Implement Fibonacci function using iteration

--- Recursion/509. Fibonacci Number.java
+++ Recursion/509. Fibonacci Number.java
@@ -1,1 +1,15 @@
-
+class Solution {
+    public int fib(int n) {
+        if (n == 0) return 0;  
+        if (n == 1) return 1;
+        int f0 = 0;
+        int f1 = 1;
+        int f2=0;
+        for(int i =2 ; i<=n ; i++){
+            f2 = f0 + f1;
+            f0 = f1;
+            f1 = f2;
+        }
+        return f2;
+    }
+}
