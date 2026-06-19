4edec24aada560535d0df46d582baf53bde45264
Rajat Kaware <rajatkaware2005@gmail.com>
2026-06-19 15:43:28 +0530

    Implement mergeTwoLists to merge two sorted lists

--- Linked List/21. Merge Two Sorted Lists.java
+++ Linked List/21. Merge Two Sorted Lists.java
@@ -1,1 +1,36 @@
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+        ListNode h1 = list1;
+        ListNode h2 = list2;
 
+       
+        while (h1 != null && h2 != null) {
+            if (h1.val <= h2.val) {
+                curr.next = h1;
+                h1 = h1.next;
+            } else {
+                curr.next = h2;
+                h2 = h2.next;
+            }
+            curr = curr.next;
+        }
+
+       
+        while (h1 != null) {
+            curr.next = h1;
+            h1 = h1.next;
+            curr = curr.next;
+        }
+
+       
+        while (h2 != null) {
+            curr.next = h2;
+            h2 = h2.next;
+            curr = curr.next;
+        }
+
+        return dummy.next;
+    }
+}
