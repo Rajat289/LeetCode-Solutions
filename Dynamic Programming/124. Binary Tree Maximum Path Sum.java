e15dd725356da9c6fd637112a3f06a179e89d49e
Rajat Kaware <rajatkaware2005@gmail.com>
2026-06-22 23:26:36 +0530

    Implement maxPathSum for binary tree maximum path sum

--- Dynamic Programming/124. Binary Tree Maximum Path Sum.java
+++ Dynamic Programming/124. Binary Tree Maximum Path Sum.java
@@ -1,1 +1,16 @@
+class Solution {
+    public int maxPathSum(TreeNode root) {
+        int[]res={root.val};
+        dfs(root,res);
+        return res[0];
+    }
+    private int dfs(TreeNode node , int[]res){
+        if(node == null)return 0;
 
+        int leftsum=Math.max(0,dfs(node.left , res));
+        int rightsum=Math.max(0,dfs(node.right , res));
+
+        res[0]=Math.max(res[0],leftsum + rightsum + node.val);
+        return node.val+Math.max(leftsum,rightsum);
+    }
+}
