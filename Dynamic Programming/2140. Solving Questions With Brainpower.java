f676b7c40b82f253c0263bbe7f7acf537caf5c5c
Rajat Kaware <rajatkaware2005@gmail.com>
2026-06-30 19:38:50 +0530

    Implement dynamic programming solution for questions

--- Dynamic Programming/2140. Solving Questions With Brainpower.java
+++ Dynamic Programming/2140. Solving Questions With Brainpower.java
@@ -1,1 +1,24 @@
+import java.util.Arrays;
 
+class Solution {
+    public long mostPoints(int[][] questions) {
+        int n = questions.length;
+        long[] dp = new long[n];  
+        Arrays.fill(dp, -1);
+        return helper(questions, 0, dp);
+    }
+
+    private long helper(int[][] questions, int i, long[] dp) {
+        if (i >= questions.length) return 0; 
+        
+        if (dp[i] != -1) return dp[i]; 
+        
+       
+        long solve = questions[i][0] + helper(questions, i + questions[i][1] + 1, dp);
+        
+       
+        long skip = helper(questions, i + 1, dp);
+        
+        return dp[i] = Math.max(solve, skip); 
+    }
+}
