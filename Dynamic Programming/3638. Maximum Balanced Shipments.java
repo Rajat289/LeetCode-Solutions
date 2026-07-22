a71376be69ed970a52d7ee975a566c8c948be428
Rajat Kaware <rajatkaware2005@gmail.com>
2026-07-22 22:21:40 +0530

    Implement maxBalancedShipments method in Solution class

--- Dynamic Programming/3638. Maximum Balanced Shipments.java
+++ Dynamic Programming/3638. Maximum Balanced Shipments.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int maxBalancedShipments(int[] weight) {
+        int len = weight.length;
+        int m = 0, count = 0;
+        long peakWeight = Long.MIN_VALUE;
+        for (int n = 1; n < len; n++) {
+            if (n > m) {
+                peakWeight = Math.max(peakWeight, weight[n - 1]);
+                if (peakWeight > weight[n]) {
+                    count++;
+                    m = n + 1;
+                    peakWeight = Long.MIN_VALUE;
+                }
+            }
+        }
+        return count;
+    }
+}
